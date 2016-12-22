f80ec96c940d60d3800feec5308528365b61cc97
viejodecaldas <viejodecaldas@github.com>
2016-12-21 23:03:49 -0500

    se añade el componente de lista de productos

---
+++ src/app/products/product-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'pm-products',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
