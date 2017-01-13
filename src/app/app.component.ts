4c4cfb9112004bf5e934e067f27aebfcdcebffa5
Miguel Eduardo Osorio Montilla <mosoriom@psl.com.co>
2017-01-13 09:08:24 -0500

    Se añade servicios para obtener datos de backend en l onNgInit()

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import {ProductListComponent} from './products/product-list.component'
+import {ProductService} from './products/product.service'
 
 @Component({
   selector: 'pm-app',
   templateUrl: './app.component.html',
-  entryComponents: [ProductListComponent]
+  entryComponents: [ProductListComponent],
+  providers: [ProductService]
 })
 export class AppComponent {
     pageTitle: string = 'Acme Product Management';
